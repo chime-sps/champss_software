ec89db7742b0189b3d75fb4b519a2761361864e2
reyniersquillace <reysquillace@gmail.com>
2024-05-29 14:45:08 +0000

    use yaml.dump, file still not showing up

--- champss/ps-processes/ps_processes/processes/make_fake.py
+++ champss/ps-processes/ps_processes/processes/make_fake.py
@@ -1,8 +1,11 @@
 from ps_inject import generate
 import click
+import yaml
+import os
+
 
 @click.argument(
-        "-n",
+        "n",
         type = int,
 )
 
@@ -26,29 +29,28 @@ def main(n, file_name, path):
     if path != 'random':
         load_profs = np.load(path)
         n = len(load_profs)
+    
+    data = []
 
-    with open(file_name, 'w') as file:
+    for i in range(n):
+        
+        n_dict = {}
 
-        for i in range(n):
+        n_dict['frequency'] = rand.choice(
+            np.linspace(0.1, 100, 10000), num_injections, replace=False
+        )
+        n_dict['DM'] = rand.choice(np.linspace(10, 200, 10000), num_injections, replace=False)
+        
+        n_dict['sigma'] = rand.choice(np.linspace(1, 20, 1000))
 
-            freq = rand.choice(
-                np.linspace(0.1, 100, 10000), num_injections, replace=False
-            )
-            dm = rand.choice(np.linspace(10, 200, 10000), num_injections, replace=False)
-            
-            sigma = rand.choice(np.linspace(1, 20, 1000))
+        if path == 'random':
+            n_dict['profile'] = ps_inject.generate()
 
-            if path == 'random':
-                prof = ps_inject.generate()
+        else:
+            n_dict['profile'] = load_profs[i]
+        
+        data.append(n_dict)
 
-            else:
-                prof = load_profs[i]
-
-            file.write(f'- frequency: {freq}\n')
-            file.write(f'  DM: {dm}\n')
-            file.write(f'  sigma: {sigma}\n')
-            file.write(f'  profile:\n')
-
-            for bin in prof:
-                file.write(f'    - {bin}\n')
-
+    file_name = os.getcwd()+'/'+file_name
+    stream = file(file_name, 'w')
+    yaml.dump(data, stream)
