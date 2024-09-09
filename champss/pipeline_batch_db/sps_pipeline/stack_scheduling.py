f2b092a4a163f038d259ea99194fd9f57e1d7557
Lars Künkel <lkuenkel@phas.ubc.ca>
2024-09-09 21:51:19 +0000

    reduced memory use

--- champss/pipeline_batch_db/sps_pipeline/stack_scheduling.py
+++ champss/pipeline_batch_db/sps_pipeline/stack_scheduling.py
@@ -3,14 +3,15 @@ import json
 import click
 from sps_databases import db_api, db_utils, models
 from sps_pipeline.workflow import (
+    clear_workflow_buckets,
     docker_swarm_pending_states,
     docker_swarm_running_states,
-    wait_for_no_tasks_in_states,
+    get_work_from_results,
     message_slack,
     schedule_workflow_job,
-    clear_workflow_buckets,
-    get_work_from_results
+    wait_for_no_tasks_in_states,
 )
+
 
 @click.command(context_settings={"help_option_names": ["-h", "--help"]})
 @click.option(
@@ -49,9 +50,9 @@ from sps_pipeline.workflow import (
     type=str,
     help="Name of the run",
 )
-def find_monthly_search_commands(db_port, db_host, db_name, day_threshold, options, run_name
+def find_monthly_search_commands(
+    db_port, db_host, db_name, day_threshold, options, run_name
 ):
-
     db = db_utils.connect(port=db_port, host=db_host, name=db_name)
     all_stacks = list(db.ps_stacks.find({"num_days_month": {"$gte": day_threshold}}))
 
@@ -60,10 +61,20 @@ def find_monthly_search_commands(db_port, db_host, db_name, day_threshold, optio
     for stack in all_stacks:
         stack_obj = models.PsStack.from_db(stack)
         pointing = stack_obj.pointing(db)
-        arguments = {"ra": pointing.ra, "dec": pointing.dec, "components": "search-monthly",  "db_port": db_port, "db_name": db_name, "db_host": db_host, **options_dict}
-        all_commands.append({"arguments": arguments, "scheduled" : False, "maxdm": pointing.maxdm})
+        arguments = {
+            "ra": pointing.ra,
+            "dec": pointing.dec,
+            "components": "search-monthly",
+            "db_port": db_port,
+            "db_name": db_name,
+            "db_host": db_host,
+            **options_dict,
+        }
+        all_commands.append(
+            {"arguments": arguments, "scheduled": False, "maxdm": pointing.maxdm}
+        )
 
-    with open(f'{run_name}.json', 'w') as file:
+    with open(f"{run_name}.json", "w") as file:
         file.write(json.dumps(all_commands, indent=4))
 
 
@@ -84,46 +95,48 @@ def find_monthly_search_commands(db_port, db_host, db_name, day_threshold, optio
     type=str,
     help="File from which the files are executed.",
 )
-def execute_monthly_search_commands( docker_image_name,
-    docker_password, command_file,
+def execute_monthly_search_commands(
+    docker_image_name,
+    docker_password,
+    command_file,
 ):
-
-    with open(command_file, 'r') as file:
+    with open(command_file) as file:
         all_commands = json.load(file)
 
     docker_mounts = [
-                        "/data/chime/sps/raw:/data/chime/sps/raw",
-                        "/data/chime/sps/sps_processing:/data/chime/sps/sps_processing",
-                    ]
+        "/data/chime/sps/raw:/data/chime/sps/raw",
+        "/data/chime/sps/sps_processing:/data/chime/sps/sps_processing",
+    ]
     workflow_function = "sps_pipeline.pipeline.stack_and_search"
     workflow_name = "champss-stack-search"
     clear_workflow_buckets(
-                [
-                    "--workflow-buckets-name",
-                    workflow_name
-                ],
-                standalone_mode=False
-            )
+        ["--workflow-buckets-name", workflow_name], standalone_mode=False
+    )
 
     for command_dict in all_commands:
         if command_dict["scheduled"] is False:
             formatted_ra = f'{command_dict["arguments"]["ra"]:.02f}'.replace(".", "_")
             formatted_dec = f'{command_dict["arguments"]["dec"]:.02f}'.replace(".", "_")
             docker_name = f"stack_search_{formatted_ra}_{formatted_dec}"
-            docker_memory_reservation = (70 + ((command_dict["maxdm"] / 100) * 4))
+            docker_memory_reservation = 30 + ((command_dict["maxdm"] / 100) * 4)
             workflow_params = command_dict["arguments"]
-            workflow_tags = ["stack_search", "monthly_search", formatted_ra, formatted_dec]
+            workflow_tags = [
+                "stack_search",
+                "monthly_search",
+                formatted_ra,
+                formatted_dec,
+            ]
             schedule_workflow_job(
-                        docker_image_name,
-                        docker_mounts,
-                        docker_name,
-                        docker_memory_reservation,
-                        docker_password,
-                        workflow_name,
-                        workflow_function,
-                        workflow_params,
-                        workflow_tags,
-                    )
+                docker_image_name,
+                docker_mounts,
+                docker_name,
+                docker_memory_reservation,
+                docker_password,
+                workflow_name,
+                workflow_function,
+                workflow_params,
+                workflow_tags,
+            )
             command_dict["scheduled"] = True
-            with open(command_file, 'w') as file:
+            with open(command_file, "w") as file:
                 file.write(json.dumps(all_commands, indent=4))
