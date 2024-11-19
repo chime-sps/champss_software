370d27c6743fb30d6124505a32f23a13095a104c
chrisfandrade16 <chrisfandrade16@gmail.com>
2024-11-19 11:19:20 -0800

    Try new GitHub Actions and Docker/Workflow example demo [test]

---
+++ champss/example.py
@@ -0,0 +1,56 @@
+from scheduler.workflow import (
+        schedule_workflow_job,
+        wait_for_no_tasks_in_states,
+        clear_workflow_buckets,
+        get_work_from_results,
+        docker_swarm_pending_states,
+        docker_swarm_running_states,
+)
+
+def example_workflow_function(my_input_value):
+    print("Running my Workflow function.")
+    return {"some_value": my_input_value}, [], []
+
+def example_submit_job():
+    workflow_buckets_name = "my-bucket"
+
+    clear_workflow_buckets.main(
+        args=["--workflow-buckets-name", workflow_buckets_name],
+        standalone_mode=False,
+    )
+    
+    docker_service_prefix = "chris"
+    docker_registry_name = "sps-archiver1.chime:5000" # DockerHub = chimefrb
+    github_repo_name = "champss_software"
+    github_branch_name = "adapt-code-for-local-docker-image-registry"
+
+    work_id = schedule_workflow_job(
+        docker_image_name=f"{docker_registry_name}/{github_repo_name}:{github_branch_name}",
+        docker_mounts=["/home/candrade:/champss_module/my_home"],
+        docker_name=f"{docker_service_prefix}-my-workflow",
+        docker_memory_reservation=5,
+        workflow_buckets_name=workflow_buckets_name,
+        workflow_function="champss.example.example_workflow_function",
+        workflow_params={"my_input_value": 42},
+        workflow_tags=["chris", "my-workflow"],
+    )
+
+    wait_for_no_tasks_in_states(
+        states_to_wait_for_none=docker_swarm_pending_states, docker_service_name_prefix=docker_service_prefix
+    )
+    print("Jobs are now running.")
+    
+    wait_for_no_tasks_in_states(
+        states_to_wait_for_none=docker_swarm_running_states, docker_service_name_prefix=docker_service_prefix
+    )
+    print("Jobs are now complete.")
+
+    work_result = get_work_from_results(
+        workflow_results_name=workflow_buckets_name,
+        work_id=work_id,
+        failover_to_buckets=True,
+    )
+    print(work_result)
+
+if __name__ == "__main__":
+    example_submit_job()
