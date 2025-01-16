91c5c9fee6e33a310400a62aacc6bf971043fa12
Laurent Tarabout <laurent.tarabout@mail.mcgill.ca>
2025-01-16 20:52:13 +0000

    Added plotting of candidates if sigma>8

--- champss/FFA/multiday_search.sh
+++ champss/FFA/multiday_search.sh
@@ -8,6 +8,6 @@
 # python FFA_search_candtest.py --psr "B0154+61" --date 20240607 --generate_candidates -s
 # python FFA_search_candtest.py --psr "B0154+61" --date 20240608 --generate_candidates -s
 # python FFA_search_candtest.py --psr "B0154+61" --date 20240609 --generate_candidates -s
-# python FFA_search_candtest.py --psr "B0154+61" --date 20240610 --generate_candidates -s
+python FFA_search_candtest.py --psr "B0154+61" --date 20240610 --generate_candidates
 
-python FFA_search_candtest.py --ra 160 --dec 50 --date 20240610 --generate_candidates -s --plot
+# python FFA_search_candtest.py --ra 160 --dec 50 --date 20240610 --generate_candidates -s --plot
