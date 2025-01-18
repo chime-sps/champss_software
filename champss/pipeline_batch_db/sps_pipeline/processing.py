e379306f3b1cb73d6c0f8d9c7612759fbdd8e35e
Laurent Tarabout <ltarabout@sps-archiver2.chime>
2025-01-18 10:51:02 +0000

    [test] Trigger github action

--- champss/pipeline_batch_db/sps_pipeline/processing.py
+++ champss/pipeline_batch_db/sps_pipeline/processing.py
@@ -579,7 +579,7 @@ def run_all_pipeline_processes(
                         "plot_threshold": 8.0,
                         "ra": process.ra,
                         "dec": process.dec,
-                        "components": ["rfi","beamform","dedisp","ffa","cleanup"],
+                        "components": "rfi beamform dedisp ffa cleanup",
                         "config_file": "champss_software/champss/FFA/sps_config_Laurent.yml",
                         "num_threads": docker_threads_needed,
                         "db_port": db_port,
