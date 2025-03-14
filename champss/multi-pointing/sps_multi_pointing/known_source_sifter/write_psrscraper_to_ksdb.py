33a27cd32112bdd69490da549a6143245ea011ed
magnuslargent1 <138926422+magnuslargent1@users.noreply.github.com>
2025-03-14 12:29:04 -0400

    116 remove duplicate psr scraper sources from ks db when updating ks database (#129)
    * feat(write_psrscraper_to_ksdb): checks if scraper source is duplicate, if yes does not add to db
    
    If the duplicate scraper source is already in the db, it gets removed
    
    * fix(README): fixed typos
    
    ---------
    
    Co-authored-by: Magnus L'Argent <magnuslargent@sps-archiver1.chime>

--- champss/multi-pointing/sps_multi_pointing/known_source_sifter/write_psrscraper_to_ksdb.py
+++ champss/multi-pointing/sps_multi_pointing/known_source_sifter/write_psrscraper_to_ksdb.py
@@ -6,6 +6,20 @@ import pulsarsurveyscraper
 from add_tzpar_sources import add_source_to_database
 from beamformer.utilities.dm import DMMap
 from sps_databases import db_api, db_utils
+from sps_databases.db_api import get_nearby_known_sources, delete_known_source
+
+def check_if_duplicate(psrname, ra, dec, P0, dm, survey):
+    sources = get_nearby_known_sources(ra, dec, 0.5)  
+    for source in sources: 
+        if survey != source.survey: # Needed in case same exact source is already in db
+            del_dm = np.abs(source.dm - dm)
+            del_spin_period = np.abs(source.spin_period_s - P0)
+            if del_dm < dm/10 and del_spin_period < P0/1000:
+                # print(f'{psrname} is a duplicate of {source.source_name}')
+                # print(source.source_name, source.pos_ra_deg, source.pos_dec_deg, source.spin_period_s, source.dm, source.survey)
+                # print(psrname, ra, dec, P0, dm, survey)
+                return True
+    return False
 
 if __name__ == "__main__":
     """
@@ -75,33 +89,42 @@ if __name__ == "__main__":
             DMerr = nan
 
         survey = data["survey"][i]
-        if dec > -20:
-            payload = {
-                "source_type": 1,
-                "source_name": psrname,
-                "pos_ra_deg": ra,
-                "pos_dec_deg": dec,
-                "pos_error_semimajor_deg": 0.068,
-                "pos_error_semiminor_deg": 0.068,
-                "pos_error_theta_deg": 0.0,
-                "dm": DM,
-                "dm_error": DMerr,
-                "spin_period_s": P0,
-                "spin_period_s_error": P0err,
-                "dm_galactic_ne_2001_max": float(dmm.get_dm_ne2001(dec, ra)),
-                "dm_galactic_ymw_2016_max": float(dmm.get_dm_ymw16(dec, ra)),
-                "spin_period_derivative": 0.0,
-                "spin_period_derivative_error": 0.0,
-                "spin_period_epoch": 39000.0,  # placeholder, older than oldest ephemeris in psrcat so as to not overwrite
-                "survey": [survey.lower(), "psr_scraper"],
-            }
-            if not args.update:
-                add_source_to_database(payload)
-            else:
-                print("Updating known source database using psrscraper")
-                ks = db.known_sources.find_one({"source_name": psrname})
-                if ks is None:
-                    add_source_to_database(payload)
-                else: 
+        if dec > -20 and survey != 'ATNF': # ATNF sources are redundant with the write_psrcat script
+            duplicate = check_if_duplicate(psrname, ra, dec, P0, DM, [survey.lower(), "psr_scraper"])
+            if duplicate: 
+                ks = db.known_sources.find_one({"source_name": psrname, "survey" : [survey.lower(), "psr_scraper"]})
+                if ks: 
+                    print(f'Removing {psrname} from database \n')
                     ks_id = ks["_id"]
-                    db_api.update_known_source(ks_id, payload)
+                    delete_known_source(ks_id)
+            else:
+                payload = {
+                    "source_type": 1,
+                    "source_name": psrname,
+                    "pos_ra_deg": ra,
+                    "pos_dec_deg": dec,
+                    "pos_error_semimajor_deg": 0.068,
+                    "pos_error_semiminor_deg": 0.068,
+                    "pos_error_theta_deg": 0.0,
+                    "dm": DM,
+                    "dm_error": DMerr,
+                    "spin_period_s": P0,
+                    "spin_period_s_error": P0err,
+                    "dm_galactic_ne_2001_max": float(dmm.get_dm_ne2001(dec, ra)),
+                    "dm_galactic_ymw_2016_max": float(dmm.get_dm_ymw16(dec, ra)),
+                    "spin_period_derivative": 0.0,
+                    "spin_period_derivative_error": 0.0,
+                    "spin_period_epoch": 39000.0,  # placeholder, older than oldest ephemeris in psrcat so as to not overwrite
+                    "survey": [survey.lower(), "psr_scraper"],
+                }
+                
+                if not args.update:
+                    add_source_to_database(payload)
+                else:
+                    print("Updating known source database using psrscraper")
+                    ks = db.known_sources.find_one({"source_name": psrname, "survey" : [survey.lower(), "psr_scraper"]})
+                    if ks is None:
+                        add_source_to_database(payload)
+                    else: 
+                        ks_id = ks["_id"]
+                        db_api.update_known_source(ks_id, payload)
