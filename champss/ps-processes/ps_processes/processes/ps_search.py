e0392da38da7f04ec18538bb85a262be0ff7df11
Rey Squillace <squillace@sps-compute1.chime>
2025-09-24 18:37:58 +0000

    removed excess lines in ps_search.py

--- champss/ps-processes/ps_processes/processes/ps_search.py
+++ champss/ps-processes/ps_processes/processes/ps_search.py
@@ -262,12 +262,9 @@ class PowerSpectraSearch:
                     injection_indices = np.arange(len(injection_df))
                 for injection_index in injection_indices:
                     log.info(f"DM: {injection_df.iloc[injection_index]['DM']}")
-                    log.info(f"sigma: {injection_df.iloc[injection_index]['sigma']}")
                     log.info(
                         f"frequency: {injection_df.iloc[injection_index]['frequency']}"
                     )
-
-                    # injection_dict = injection_list[injection_index]
                     injection_dict = injection_df.iloc[injection_index].to_dict()
 
                     injection_dict = ps_inject.main(
