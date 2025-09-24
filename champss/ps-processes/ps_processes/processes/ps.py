16fea2f92a9a7c6ae54087c6f2b42e428bdaa1db
Rey Squillace <squillace@sps-compute1.chime>
2025-09-24 18:35:23 +0000

    took out unnecessary print line

--- champss/ps-processes/ps_processes/processes/ps.py
+++ champss/ps-processes/ps_processes/processes/ps.py
@@ -317,7 +317,6 @@ class PowerSpectraCreation:
 
             if self.save_medians:
                 rn_medians = medians[np.newaxis, :]
-                print(rn_medians)
                 rn_dm_indices = median_dm_indices[np.newaxis, :]
                 # note that scales are saved iteratively over DM but are identical at each DM
                 # so we only need one row of scales
