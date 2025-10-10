5a5e79591928caf27a55754a0310b2ad451c19c7
Rey Squillace <squillace@sps-compute1.chime>
2025-10-10 17:40:25 +0000

    Save freq_labels to medians.npz

--- champss/ps-processes/ps_processes/processes/ps.py
+++ champss/ps-processes/ps_processes/processes/ps.py
@@ -314,6 +314,7 @@ class PowerSpectraCreation:
                 medians = np.asarray(medians)
                 median_dm_indices = np.asarray(median_dm_indices)
                 scales = np.asarray(scales)
+                freq_labels = self.freq_labels[np.cumsum(scales)]
 
             if self.save_medians:
                 rn_medians = medians[np.newaxis, :]
@@ -333,6 +334,7 @@ class PowerSpectraCreation:
                     medians_path,
                     medians=medians,
                     scales=scales,
+                    freq_lables=freq_labels
                 )
             else:
                 medians_path = None
