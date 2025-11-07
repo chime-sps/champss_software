071080e43141f91dd0f42d6e77894c830c9d4492
Lars Künkel <lkuenkel@phas.ubc.ca>
2025-11-07 15:22:46 +0000

    run batched

--- champss/sps-pipeline/sps_pipeline/dummy_processing_workflow_batched_service.py
+++ champss/sps-pipeline/sps_pipeline/dummy_processing_workflow_batched_service.py
@@ -3,11 +3,13 @@ import logging
 import tqdm
 import time
 import docker
+from multiprocessing import Pool
 
 log_stream = logging.StreamHandler()
 logging.root.addHandler(log_stream)
 log = logging.getLogger(__name__)
 
+from workflow.definitions.work import Work
 from sps_databases import db_utils
 from scheduler.workflow import (
     schedule_workflow_job,
@@ -33,6 +35,26 @@ docker_swarm_finished_states = [
     "remove",
 ]
 
+def deposit_dummy_work(pointing):
+    work = Work(
+        pipeline="dummy-schedule", site="chime", user="CHAMPSS"
+    )
+
+    work.function = "run-dummy-task"
+    work.parameters = {}
+    work.tags = [pointing['_id'].__str__()]
+    work.config.archive.results = True
+    work.config.archive.plots = "bypass"
+    work.config.archive.products = "bypass"
+    work.retries = 1
+    # work.timeout = timeout
+
+    # wait_for_no_tasks_in_states(docker_swarm_pending_states)
+
+    work_id = work.deposit(return_ids=True)
+    return work_id
+
+
 def run_dummy_processing():
     clear_workflow_buckets.main(
                     args=["--workflow-buckets-name", "dummy-schedule"],
@@ -41,36 +63,66 @@ def run_dummy_processing():
     db = db_utils.connect()
     docker_client = docker.from_env()
     all_pointings = list(db.pointings.find())
+    all_pointings = all_pointings[:500]
     for index in range(len(all_pointings)):
         current_pointing = all_pointings[index]
         all_pointings[index]["ram_requirement"] = ram_requirement(current_pointing)
     log.info("Start Scheduling")
-    sim_wait = 10
+
+    # for pointing_index, pointing in tqdm.tqdm(enumerate(all_pointings), total=len(all_pointings)):
+    #     work = Work(
+    #         pipeline="dummy-schedule", site="chime", user="CHAMPSS"
+    #     )
+
+    #     work.function = "run-dummy-task"
+    #     work.parameters = {}
+    #     work.tags = [pointing['_id'].__str__()]
+    #     work.config.archive.results = True
+    #     work.config.archive.plots = "bypass"
+    #     work.config.archive.products = "bypass"
+    #     work.retries = 1
+    #     # work.timeout = timeout
+
+    #     # wait_for_no_tasks_in_states(docker_swarm_pending_states)
+
+    #     work_id = work.deposit(return_ids=True)
+    with Pool(16) as pool:
+        work_ids = list(tqdm.tqdm(pool.imap(deposit_dummy_work, all_pointings), total=len(all_pointings)))
+
+    pointing = all_pointings[0]
+
+    log.info("Filled the bucket")
+    # breakpoint()
+    sim_wait = 1
     cleanup_interval = 30
     waiting_services = []
     non_finished_services = []
 
     a= time.time()
-    for pointing_index, pointing in tqdm.tqdm(enumerate(all_pointings), total=len(all_pointings)):
+    work_is_not_done = False
+    # for pointing_index, pointing in tqdm.tqdm(enumerate(all_pointings), total=len(all_pointings)):
+    for i in range(5):
         # while len(waiting_services) > sim_wait:
-        while len(waiting_services) > sim_wait and pointing_index % sim_wait == 0:
-            new_waiting_services = []
-            for service_index, service in enumerate(waiting_services):
-                service.reload()
-                service_state = service.tasks()[0]["Status"]["State"]
-                if service_state in docker_swarm_pending_states:
-                    new_waiting_services.append(service)
-            waiting_services = new_waiting_services
-        if pointing_index % cleanup_interval == 1:
-            finished_indices = []
-            for service_index, service in enumerate(non_finished_services):
-                service.reload()
-                service_state = service.tasks()[0]["Status"]["State"]
-                if service_state in docker_swarm_finished_states:
-                    service.remove()
-                    finished_indices.append(service_index)
-            for service_index in finished_indices[::-1]:
-                del non_finished_services[service_index]
+        # while len(waiting_services) > sim_wait:
+        #     new_waiting_services = []
+        #     for service_index, service in enumerate(waiting_services):
+        #         service.reload()
+        #         service_state = service.tasks()[0]["Status"]["State"]
+        #         if service_state in docker_swarm_pending_states:
+        #             new_waiting_services.append(service)
+        #         else:
+        #             log.info("Removed finished service")
+        #     waiting_services = new_waiting_services
+        # if pointing_index % cleanup_interval == 1:
+        #     finished_indices = []
+        #     for service_index, service in enumerate(non_finished_services):
+        #         service.reload()
+        #         service_state = service.tasks()[0]["Status"]["State"]
+        #         if service_state in docker_swarm_finished_states:
+        #             service.remove()
+        #             finished_indices.append(service_index)
+        #     for service_index in finished_indices[::-1]:
+        #         del non_finished_services[service_index]
         
 
                 
@@ -109,10 +161,14 @@ def run_dummy_processing():
             "image": "sps-archiver1.chime:5000/champss_software:dummy_scheduling",
             # Can't have dots or slashes in Docker Service names
             # All Docker Services made with this function will be prefixed with "processing-"
-            "name": f"processing-{pointing['_id'].__str__()}",
+            "name": f"processing-{i}",
             # Use one-shot Workflow runners since we need a new container per process for unique memory reservations
             # (we currently only use Workflow as a wrapper for its additional features, e.g. frontend)
-            "command": "run-dummy-task",
+            "command": (
+                "workflow run"
+                f" dummy-schedule --site"
+                f" chime --lives 100 --sleep 1"
+            ),
             "mode": docker.types.ServiceMode("replicated", replicas=1),
             "restart_policy": docker.types.RestartPolicy(
                 condition="none", max_attempts=0
@@ -145,6 +201,15 @@ def run_dummy_processing():
     b = time.time()
     log.info("Finish Scheduling")
     log.info(b-a)
+    all_states = ["started"]
+    while not set(all_states).issubset(docker_swarm_finished_states):
+        all_states = []
+        for service in non_finished_services:
+            all_states.append(service.tasks()[0]["Status"]["State"])
+    c = time.time()
+    log.info("Prcessing finished")
+    log.info(c-a)
+    log.info((c-a)/len(all_pointings))
 
 
 def ram_requirement(pointing_dict):
