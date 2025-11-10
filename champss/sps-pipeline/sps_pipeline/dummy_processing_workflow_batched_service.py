9295bc128421f781b73b9d32ddcef3f894056cb5
Lars Künkel <lkuenkel@phas.ubc.ca>
2025-11-10 17:58:24 +0000

    pretty much working version

--- champss/sps-pipeline/sps_pipeline/dummy_processing_workflow_batched_service.py
+++ champss/sps-pipeline/sps_pipeline/dummy_processing_workflow_batched_service.py
@@ -1,6 +1,5 @@
 # Code to benchmark overhead of processing
 import logging
-import tqdm
 import time
 import docker
 from multiprocessing import Pool
@@ -11,11 +10,8 @@ log = logging.getLogger(__name__)
 
 from workflow.definitions.work import Work
 from sps_databases import db_utils
-from scheduler.workflow import (
-    schedule_workflow_job,
-    clear_workflow_buckets,
-    wait_for_no_tasks_in_states
-)
+from scheduler.workflow import clear_workflow_buckets
+import pymongo
 
 docker_swarm_pending_states = [
     "new",
@@ -35,7 +31,9 @@ docker_swarm_finished_states = [
     "remove",
 ]
 tiers = ["small", "large"]
-mem_ranges = [[0,10], [10,1000]]
+mem_ranges = [[0, 11], [11, 1000]]
+mem_reserves = [11, 50]
+
 
 def get_tier(mem_req):
     tier = -1
@@ -45,18 +43,15 @@ def get_tier(mem_req):
             break
     return tier
 
-    
 
 def deposit_dummy_work(pointing):
-    work = Work(
-        pipeline="dummy-schedule", site="chime", user="CHAMPSS"
-    )
+    work = Work(pipeline="dummy-schedule", site="chime", user="CHAMPSS")
 
     work.function = "scheduler.utils.dummy_workflow_task"
-    mem_req =ram_requirement(pointing)
-    work.parameters = {"wait_time": mem_req *1/100}
+    mem_req = ram_requirement(pointing)
+    work.parameters = {"wait_time": mem_req}
     tier = get_tier(mem_req)
-    work.tags = [pointing['_id'].__str__(), tiers[tier]]
+    work.tags = [pointing["_id"].__str__(), tiers[tier]]
     work.config.archive.results = True
     work.config.archive.plots = "bypass"
     work.config.archive.products = "bypass"
@@ -71,13 +66,13 @@ def deposit_dummy_work(pointing):
 
 def run_dummy_processing():
     clear_workflow_buckets.main(
-                    args=["--workflow-buckets-name", "dummy-schedule"],
-                    standalone_mode=False,
-                )
+        args=["--workflow-buckets-name", "dummy-schedule"],
+        standalone_mode=False,
+    )
     db = db_utils.connect()
     docker_client = docker.from_env()
     all_pointings = list(db.pointings.find())
-    all_pointings = all_pointings[:100]
+    all_pointings = all_pointings[:50000]
     for index in range(len(all_pointings)):
         current_pointing = all_pointings[index]
         all_pointings[index]["ram_requirement"] = ram_requirement(current_pointing)
@@ -100,8 +95,10 @@ def run_dummy_processing():
     #     # wait_for_no_tasks_in_states(docker_swarm_pending_states)
 
     #     work_id = work.deposit(return_ids=True)
-    with Pool(16) as pool:
-        work_ids = list(tqdm.tqdm(pool.imap(deposit_dummy_work, all_pointings), total=len(all_pointings)))
+    # with Pool(128) as pool:
+    #     work_ids = list(tqdm.tqdm(pool.imap(deposit_dummy_work, all_pointings), total=len(all_pointings)))
+    pool = Pool(32)
+    work_ids = pool.imap(deposit_dummy_work, all_pointings)
 
     pointing = all_pointings[0]
 
@@ -112,10 +109,11 @@ def run_dummy_processing():
     waiting_services = []
     non_finished_services = []
 
-    a= time.time()
+    a = time.time()
     work_is_not_done = False
     # for pointing_index, pointing in tqdm.tqdm(enumerate(all_pointings), total=len(all_pointings)):
-    for i in range(5):
+    services = []
+    for i in range(len(tiers)):
         # while len(waiting_services) > sim_wait:
         # while len(waiting_services) > sim_wait:
         #     new_waiting_services = []
@@ -137,9 +135,6 @@ def run_dummy_processing():
         #             finished_indices.append(service_index)
         #     for service_index in finished_indices[::-1]:
         #         del non_finished_services[service_index]
-        
-
-                
 
         docker_mounts = [
             f"{'/mnt/beegfs-client/'}:{'/mnt/beegfs-client/'}",
@@ -175,14 +170,14 @@ def run_dummy_processing():
             "image": "sps-archiver1.chime:5000/champss_software:dummy_scheduling",
             # Can't have dots or slashes in Docker Service names
             # All Docker Services made with this function will be prefixed with "processing-"
-            "name": f"processing-{i}",
+            "name": f"processing-{tiers[i]}",
             # Use one-shot Workflow runners since we need a new container per process for unique memory reservations
             # (we currently only use Workflow as a wrapper for its additional features, e.g. frontend)
             "command": (
                 "workflow run"
                 f" dummy-schedule --site"
-                f" chime --lives 100 --sleep 1"
-                f" --tag {tiers[i%2]}"
+                f" chime --lives -1 --sleep 1"
+                f" --tag {tiers[i % 2]}"
             ),
             "mode": docker.types.ServiceMode("replicated", replicas=1),
             "restart_policy": docker.types.RestartPolicy(
@@ -194,7 +189,7 @@ def run_dummy_processing():
             "constraints": ["node.labels.compute == true"],
             # Must be in bytes
             "resources": docker.types.Resources(
-                mem_reservation=int( pointing["ram_requirement"] * 1e9)
+                mem_reservation=int(mem_reserves[i] * 1e9)
             ),
             # Will throw an error if you give two of the same bind mount paths
             # e.g. avoid double-mounting basepath and stackpath when they are the same
@@ -207,24 +202,67 @@ def run_dummy_processing():
 
         log.info(f"Creating Docker Service: \n{docker_service}")
 
-
         service = docker_client.services.create(**docker_service)
-        waiting_services.append(service)
-        non_finished_services.append(service)
+        services.append(service.attrs["ID"])
+        # waiting_services.append(service)
+        # non_finished_services.append(service)
         # breakpoint()
         # wait_for_no_tasks_in_states(docker_swarm_pending_states, docker_service_name_prefix=f"processing-{pointing['_id'].__str__()}")
     b = time.time()
-    log.info("Finish Scheduling")
-    log.info(b-a)
-    all_states = ["started"]
-    while not set(all_states).issubset(docker_swarm_finished_states):
-        all_states = []
-        for service in non_finished_services:
-            all_states.append(service.tasks()[0]["Status"]["State"])
+    requested_containers = 10
+    # first_works = [work for work in work_ids]
+    for work_index, work in enumerate(work_ids):
+        if work_index > requested_containers:
+            break
+    # http = HTTPContext()
+    # all_works = http.buckets.view(query={"pipeline": "dummy-schedule"}, projection={}, limit=300000)[::-1]
+    buckets_db = pymongo.MongoClient(port=27018).work.buckets
+    all_works = list(
+        buckets_db.find({"pipeline": "dummy-schedule"})
+        .sort("creation", pymongo.ASCENDING)
+        .limit(requested_containers)
+    )
+    while len(all_works) > 0:
+        upcoming_tags = {tier: 0 for tier in tiers}
+        for i, work in enumerate(all_works[:]):
+            current_tag = set(work["tags"]).intersection(set(tiers))
+            current_tag = [tag for tag in current_tag][0]
+            upcoming_tags[current_tag] += 1
+        print(upcoming_tags)
+        print(all_works[0]["creation"])
+        # breakpoint()
+        for i, tier in enumerate(tiers):
+            # services[i].scale(upcoming_tags[tier])
+            docker_client.services.get(services[i]).scale(upcoming_tags[tier])
+        time.sleep(5)
+        running_tasks = 0
+        for i, tier in enumerate(tiers):
+            service_tasks = docker_client.services.get(services[i]).tasks()
+            running_tasks += sum(
+                1 for task in service_tasks if task["Status"]["State"] == "running"
+            )
+        print(running_tasks)
+        requested_containers = running_tasks + 10
+        all_works = list(
+            buckets_db.find({"pipeline": "dummy-schedule"})
+            .sort("creation", pymongo.ASCENDING)
+            .limit(requested_containers)
+        )
+    for i, tier in enumerate(tiers):
+        # services[i].scale(upcoming_tags[tier])
+        docker_client.services.get(services[i]).scale(0)
+
+    # log.info("Finish Scheduling")
+    log.info(b - a)
+    # all_states = ["started"]
+    # while not set(all_states).issubset(docker_swarm_finished_states):
+    #     all_states = []
+    #     for service in non_finished_services:
+    #         all_states.append(service.tasks()[0]["Status"]["State"])
     c = time.time()
     log.info("Prcessing finished")
-    log.info(c-a)
-    log.info((c-a)/len(all_pointings))
+    log.info(c - a)
+    log.info((c - a) / len(all_pointings))
 
 
 def ram_requirement(pointing_dict):
