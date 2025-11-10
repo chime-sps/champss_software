568fe84913d8584be97ba9841ffac760b48a0992
Lars Künkel <lkuenkel@phas.ubc.ca>
2025-11-10 19:31:37 +0000

    [test] fix typo

--- champss/sps-pipeline/sps_pipeline/processing.py
+++ champss/sps-pipeline/sps_pipeline/processing.py
@@ -958,10 +958,10 @@ def run_all_pipeline_processes(
     help="To run the multipointing phase of processing or not.",
 )
 @click.option(
-    "--run-classfication",
+    "--run-classification",
     default=True,
     type=bool,
-    help="To run the classfication phase of processing or not.",
+    help="To run the classification phase of processing or not.",
 )
 @click.option(
     "--run-folding",
