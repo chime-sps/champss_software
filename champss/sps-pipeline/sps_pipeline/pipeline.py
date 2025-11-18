12afe45b48f3ec53faa095a74cbc3620c246fd84
Lars Künkel <lkuenkel@phas.ubc.ca>
2025-11-18 19:44:28 +0000

    [test] try statement around remove handler

--- champss/sps-pipeline/sps_pipeline/pipeline.py
+++ champss/sps-pipeline/sps_pipeline/pipeline.py
@@ -108,9 +108,12 @@ def apply_logging_config(config, log_file="./logs/default.log"):
 
     if config.logging.get("file_logging", False):
         # Remove possibly pre-existing file handler
-        for log_handler in log.root.handlers:
-            if isinstance(log_handler, logging.FileHandler):
-                log.root.removeHandler(log_handler)
+        try:
+            for log_handler in log.root.handlers:
+                if isinstance(log_handler, logging.FileHandler):
+                    log.root.removeHandler(log_handler)
+        except Exception as error:
+            log.info(f"Could not remove file handler due to {error}")
         os.makedirs(os.path.dirname(log_file), exist_ok=True)
         file_handler = logging.FileHandler(log_file)
         file_handler.setFormatter(
