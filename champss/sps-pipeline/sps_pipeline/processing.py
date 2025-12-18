57d5bc2213908955ff37446027be7a5148933bec
Lars Künkel <lkuenkel@phas.ubc.ca>
2025-12-18 19:36:01 +0000

    fix typo

--- champss/sps-pipeline/sps_pipeline/processing.py
+++ champss/sps-pipeline/sps_pipeline/processing.py
@@ -1622,7 +1622,7 @@ def start_processing_manager(
                             {"folding_result": {"fold_count": len(processes)}},
                         )
                 except Exception as error:
-                    log.error("Could not update daily candidates due to f{error}")
+                    log.error(f"Could not update daily candidates due to {error}")
                     log.error(traceback.format_exc())
             # End of folding phase
 
