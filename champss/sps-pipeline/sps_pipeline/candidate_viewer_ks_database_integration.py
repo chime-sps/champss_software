609ba981827ed54afff2b7500647ca4df26d6454
Lars Künkel <lkuenkel@phas.ubc.ca>
2026-02-26 16:59:49 +0000

    Sanity check for added parameters

--- champss/sps-pipeline/sps_pipeline/candidate_viewer_ks_database_integration.py
+++ champss/sps-pipeline/sps_pipeline/candidate_viewer_ks_database_integration.py
@@ -1,4 +1,5 @@
 import click
+import numpy as np
 
 import sps_pipeline.candidate_viewer as cv
 from sps_databases import db_utils, db_api
@@ -52,9 +53,7 @@ def move_derived_values_to_ks_db(
         "port": 3306,
     }
     cqv = cv.CandidateViewerQuery(survey, db_config)
-    # if folder:
-    #     all_psr_cands = cqv.get_ratings(folder=folder, classification="<any_known>", with_metadata=True)
-    # else:
+
     all_psr_cands = cqv.get_ratings(
         folder=folder, classification="<any_known>", with_metadata=True
     )
@@ -68,7 +67,11 @@ def move_derived_values_to_ks_db(
         else:
             print(f"{site_cand['result']} not found in db.")
             continue
-        cand = MultiPointingCandidate.read(site_cand["metadata"]["input_file"])
+        try:
+            cand = MultiPointingCandidate.read(site_cand["metadata"]["input_file"])
+        except FileNotFoundError:
+            print(f"{site_cand['metadata']['input_file']} not found.")
+            continue
         champss_derived_parameters = {
             "dm": cand.best_dm,
             "dm_error": cand.best_candidate_features[
@@ -82,6 +85,22 @@ def move_derived_values_to_ks_db(
             # "pos_error_semimajor_deg": float(cand.position_features["std_ra"]),
             # "pos_error_semiminor_deg": float(cand.position_features["std_dec"]),
         }
+        for value in champss_derived_parameters.values():
+            if not np.isfinite(value):
+                print(f"Parameters for {site_cand['result']} contain nan.")
+                continue
+        if champss_derived_parameters["dm_error"] > 20:
+            print(
+                f"DM Error for {site_cand['result']} too big ({champss_derived_parameters['dm_error']})."
+            )
+            continue
+        freq_error = cand.best_candidate_features[
+            "freq_sigma_FitGaussWidth_gauss_sigma"
+        ][0]
+        if freq_error > 0.002:
+            print(f"Frequency error for {site_cand['result']} too big ({freq_error}).")
+            continue
+
         payload = {"champss_derived_parameters": champss_derived_parameters}
         db_api.update_known_source(ks._id, payload)
         print(f"Updated {site_cand['result']} with {champss_derived_parameters}")
