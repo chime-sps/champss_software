adc9ecd4d52880f93377dd11f6dac234aa79076d
larskuenkel <larskuenkel@users.noreply.github.com>
2026-03-19 11:04:06 -0400

    Make stopping beams more reliable (#235)
    * [test] removed try statement
    
    * [test] add schedulign to package
    
    * rerurn twice and set timeout to 10 seconds

--- champss/controller/controller/__init__.py
+++ champss/controller/controller/__init__.py
@@ -14,7 +14,7 @@ import click
 import trio
 from controller.l1_rpc import get_beam_ip, get_node_beams
 from controller.pointer import generate_pointings
-from controller.updater import pointing_beam_control, timeout
+from controller.updater import pointing_beam_control
 from scheduling.scheduleknownpulsars import is_beam_recording
 
 log = logging.getLogger("spsctl")
@@ -61,29 +61,25 @@ async def entry_point(active_beams, basepath, source="champss"):
     """
     pointing_done_announce, pointing_done_listen = trio.open_memory_channel(math.inf)
     beam_schedule_channels = {}
-    try:
-        async with trio.open_nursery() as nursery:
-            log.debug("strat: spawning announcer...")
-            nursery.start_soon(announce_pointing_done, pointing_done_listen)
-            for beam_id in active_beams:
-                new_pointing_announce, new_pointing_listen = trio.open_memory_channel(
-                    math.inf
-                )
-                log.debug(f"strat: spawning controller for beam {beam_id}...")
-                nursery.start_soon(
-                    pointing_beam_control,
-                    new_pointing_listen,
-                    pointing_done_announce.clone(),
-                    basepath,
-                    source,
-                )
-                beam_schedule_channels[beam_id] = new_pointing_announce
+    async with trio.open_nursery() as nursery:
+        log.debug("strat: spawning announcer...")
+        nursery.start_soon(announce_pointing_done, pointing_done_listen)
+        for beam_id in active_beams:
+            new_pointing_announce, new_pointing_listen = trio.open_memory_channel(
+                math.inf
+            )
+            log.debug(f"strat: spawning controller for beam {beam_id}...")
+            nursery.start_soon(
+                pointing_beam_control,
+                new_pointing_listen,
+                pointing_done_announce.clone(),
+                basepath,
+                source,
+            )
+            beam_schedule_channels[beam_id] = new_pointing_announce
 
-            log.debug(f"strat: spawning pointer for {active_beams}...")
-            nursery.start_soon(generate_pointings, active_beams, beam_schedule_channels)
-    except BaseExceptionGroup:
-        pass
-        # When cancelling the batch acquisition this Exception will be thrown by trio.
+        log.debug(f"strat: spawning pointer for {active_beams}...")
+        nursery.start_soon(generate_pointings, active_beams, beam_schedule_channels)
 
 
 def start_beam(beam: int, basepath: str, source: str, channels: int, ntime: int):
@@ -128,7 +124,9 @@ def stop_beam(beam: int, basepath: str, source: str):
     return proc
 
 
-def stop_all_beams(active_beams, basepath, source="champss", batchsize=20):
+def stop_all_beams(
+    active_beams, basepath, source="champss", batchsize=20, rerun_count=2
+):
     """
     Stop all beams.
 
@@ -137,19 +135,28 @@ def stop_all_beams(active_beams, basepath, source="champss", batchsize=20):
     """
     # Creating processes for all beams at once will lead to a crash
     batched_beams = batched(active_beams, batchsize)
+    failed_beams = []
     for beam_batch in batched_beams:
         procs = [stop_beam(beam, basepath, source) for beam in beam_batch]
         time.sleep(0.1)
         for proc, beam in zip(procs, beam_batch):
             try:
-                output = proc.communicate(timeout=5)
+                output = proc.communicate(timeout=10)
                 if "parameters successfully applied" in str(output[0]):
                     output = f"Successfully stopped {beam}"
             except subprocess.TimeoutExpired as e:
                 output = f"Unable to stop beam {beam}"
+                failed_beams.append(beam)
                 os.killpg(os.getpgid(proc.pid), signal.SIGTERM)
             finally:
                 log.info(output)
+    if len(failed_beams):
+        if rerun_count:
+            stop_all_beams(
+                failed_beams, basepath, source, batchsize, rerun_count=rerun_count - 1
+            )
+        else:
+            log.error(f"Could not stop beams {failed_beams}.")
 
 
 @click.command(context_settings={"help_option_names": ["-h", "--help"]})
