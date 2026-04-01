a88e377347ffb4612ae208cea6b2d2510748b4ec
Rey Squillace <squillace@sps-compute1.chime>
2026-04-01 15:18:42 +0000

    switch to mean normalization method

--- champss/ps-processes/ps_processes/processes/ps_inject.py
+++ champss/ps-processes/ps_processes/processes/ps_inject.py
@@ -468,15 +468,9 @@ class Injection:
         """
         rn_scales = self.pspec_obj.rn_scales
         rn_medians = self.pspec_obj.rn_medians
-        normalizer = np.zeros((len(inj_dms), len(inj_bins)))
+        sum_of_medians = np.zeros((len(inj_dms), len(inj_bins)))
 
         for day in range(self.pspec_obj.num_days):
-            day_normalizer = (
-                np.ones((len(inj_dms), len(inj_bins)))
-                #/ self.pspec_obj.num_days #why was this here???
-                / np.log(2)
-            )
-            # day_medians = rn_medians[day]
             day_medians = (
                 rn_medians[day] / np.min(rn_medians[day], axis=1)[:, np.newaxis]
             )
@@ -488,9 +482,14 @@ class Injection:
             mid_bins = ((scale_sum[1:] + scale_sum[:-1]) / 2).astype("int")
             for i, inj_dm in enumerate(inj_dms):
                 rn_interpolated = np.interp(inj_bins, mid_bins, day_medians[inj_dm])
-                day_normalizer[i] /= rn_interpolated
-            normalizer += day_normalizer
-        return normalizer
+                sum_of_medians[i] += rn_interpolated
+
+        #convert median to mean
+        sum_of_means = sum_of_medians / np.log(2)
+        #take mean across days
+        mean_of_means = sum_of_means / self.pspec_obj.num_days
+
+        return mean_of_means
 
     def predict_sigma(self, harms, bins, dm_indices, used_nharm, add_expected_mean):
         """
@@ -630,7 +629,7 @@ class Injection:
         # note that harms are POWERS, not amplitudes
 
         harms = np.asarray(harms)
-        harms *= self.get_rednoise_normalisation(
+        harms /= self.get_rednoise_normalisation(
             bins,
             dm_indices,
         )
