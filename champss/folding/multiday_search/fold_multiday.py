de5376c5e64e9782f1fa8107c68248fec556329c
Lars Künkel <lkuenkel@phas.ubc.ca>
2026-04-13 18:29:35 +0000

    [test] skip before creating service

--- champss/folding/multiday_search/fold_multiday.py
+++ champss/folding/multiday_search/fold_multiday.py
@@ -141,6 +141,10 @@ def main(
 ):
     db = db_utils.connect(host=db_host, port=db_port, name=db_name)
     source = db_api.get_followup_source(fs_id)
+    if source.folding_history:
+        fold_dates = [entry["date"].date() for entry in source.folding_history]
+    else:
+        fold_dates = []
     ra = source.ra
     dec = source.dec
     dm = source.dm
@@ -151,6 +155,10 @@ def main(
     )
     print(f"Folding {len(dates_with_data)} days of data: {dates_with_data}")
     for i, date in enumerate(dates_with_data):
+        if date.strftime("%Y%m%d") in fold_dates:
+            print(f"Already folded on {date.date()}, skipping...")
+            continue
+
         if use_workflow:
             docker_name = f"{docker_service_name_prefix}-{date}-{fs_id}"
             docker_memory_reservation = (nchan / 1024) * 8
